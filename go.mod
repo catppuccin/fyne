ab39a837d87c75179f502f2cbe5b8c2b85c9f6f7
mbaklor <mbaklor@gmail.com>
2025-03-04 17:49:42 +0200

    working theme

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/mbaklor/fyne-catppuccin
+
+go 1.24.0
+
+require (
+	fyne.io/fyne/v2 v2.5.4
+	github.com/catppuccin/go v0.3.0
+)
+
+require (
+	github.com/fredbi/uri v1.1.0 // indirect
+	github.com/fyne-io/gl-js v0.0.0-20220119005834-d2da28d9ccfe // indirect
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
+	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
+	golang.org/x/image v0.18.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
---
+++ theme.go
@@ -0,0 +1,185 @@
+package catppuccin
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
+	ctp "github.com/catppuccin/go"
+)
+
+type Flavor ctp.Flavor
+
+var (
+	Latte     = ctp.Latte
+	Frappe    = ctp.Frappe
+	Macchiato = ctp.Macchiato
+	Mocha     = ctp.Mocha
+)
+
+type Color string
+
+const (
+	Rosewater Color = "rosewater"
+	Flamingo  Color = "flamingo"
+	Pink      Color = "pink"
+	Mauve     Color = "mauve"
+	Red       Color = "red"
+	Maroon    Color = "maroon"
+	Peach     Color = "peach"
+	Yellow    Color = "yellow"
+	Green     Color = "green"
+	Teal      Color = "teal"
+	Sky       Color = "sky"
+	Sapphire  Color = "sapphire"
+	Blue      Color = "blue"
+	Lavender  Color = "lavender"
+)
+
+func getAccent(f Flavor, c Color) ctp.Color {
+	switch c {
+	case Rosewater:
+		return f.Rosewater()
+	case Flamingo:
+		return f.Flamingo()
+	case Pink:
+		return f.Pink()
+	case Mauve:
+		return f.Mauve()
+	case Red:
+		return f.Red()
+	case Maroon:
+		return f.Maroon()
+	case Peach:
+		return f.Peach()
+	case Yellow:
+		return f.Yellow()
+	case Green:
+		return f.Green()
+	case Teal:
+		return f.Teal()
+	case Sky:
+		return f.Sky()
+	case Sapphire:
+		return f.Sapphire()
+	case Blue:
+		return f.Blue()
+	case Lavender:
+		return f.Lavender()
+	}
+	return f.Blue()
+}
+
+func colorWithAlpha(c ctp.Color, a uint8) color.Color {
+	return color.NRGBA{c.RGB[0], c.RGB[1], c.RGB[2], a}
+}
+
+type Theme struct {
+	flavor Flavor
+	accent Color
+}
+
+func (ctp *Theme) ResetFlavor() {
+	ctp.flavor = nil
+}
+
+func (ctp *Theme) SetFlavor(f Flavor) {
+	ctp.flavor = f
+}
+
+func (ctp *Theme) SetAccent(c Color) {
+	ctp.accent = c
+}
+
+func (c *Theme) GetFlavor(variant fyne.ThemeVariant) (Flavor, fyne.ThemeVariant) {
+	if c.flavor == nil {
+		if variant == theme.VariantLight {
+			return ctp.Latte, variant
+		} else {
+			return ctp.Mocha, variant
+		}
+	} else {
+		v := theme.VariantDark
+		if c.flavor == Latte {
+			v = theme.VariantLight
+		}
+		return c.flavor, v
+	}
+}
+
+var _ fyne.Theme = (*Theme)(nil)
+
+func (c Theme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
+	f, v := c.GetFlavor(variant)
+	accent := f.Blue()
+	if c.accent != "" {
+		accent = getAccent(f, c.accent)
+	}
+	switch name {
+	case theme.ColorNameBackground:
+		return f.Base()
+	case theme.ColorNameButton:
+		return f.Mantle()
+	case theme.ColorNameDisabledButton:
+		return f.Surface0()
+	case theme.ColorNameDisabled:
+		return f.Text()
+	case theme.ColorNameError:
+		return f.Red()
+	case theme.ColorNameFocus:
+		return colorWithAlpha(accent, 0x2a)
+	case theme.ColorNameForeground:
+		return f.Text()
+	case theme.ColorNameForegroundOnError:
+		return f.Base()
+	case theme.ColorNameForegroundOnPrimary:
+		return f.Base()
+	case theme.ColorNameForegroundOnSuccess:
+		return f.Base()
+	case theme.ColorNameForegroundOnWarning:
+		return f.Base()
+	case theme.ColorNameHeaderBackground:
+		return f.Surface0()
+	case theme.ColorNameHover:
+		return colorWithAlpha(f.Overlay2(), 0x0f)
+	case theme.ColorNameHyperlink:
+		return f.Blue()
+	case theme.ColorNameInputBackground:
+		return f.Mantle()
+	case theme.ColorNameInputBorder:
+		return f.Surface0()
+	case theme.ColorNameMenuBackground:
+		return f.Mantle()
+	case theme.ColorNameOverlayBackground:
+		return f.Overlay0()
+	case theme.ColorNamePlaceHolder:
+		return f.Overlay2()
+	case theme.ColorNamePressed:
+		return colorWithAlpha(f.Surface0(), 0x66)
+	case theme.ColorNamePrimary:
+		return accent
+	case theme.ColorNameScrollBar:
+		return colorWithAlpha(f.Mantle(), 0x99)
+	case theme.ColorNameSelection:
+		return colorWithAlpha(accent, 0x40)
+	case theme.ColorNameSeparator:
+		return f.Crust()
+	case theme.ColorNameShadow:
+		return theme.DefaultTheme().Color(name, v)
+	case theme.ColorNameSuccess:
+		return f.Green()
+	case theme.ColorNameWarning:
+		return f.Yellow()
+	}
+	return theme.DefaultTheme().Color(name, variant)
+}
+
+func (ctp Theme) Font(style fyne.TextStyle) fyne.Resource {
+	return theme.DefaultTheme().Font(style)
+}
+func (ctp Theme) Icon(name fyne.ThemeIconName) fyne.Resource {
+	return theme.DefaultTheme().Icon(name)
+}
+func (ctp Theme) Size(name fyne.ThemeSizeName) float32 {
+	return theme.DefaultTheme().Size(name)
+}
